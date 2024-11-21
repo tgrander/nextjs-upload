6008f1c13202c27b77ace393314292259e4e2e45
Trey Granderson <treygranderson@gmail.com>
2024-11-21 17:17:59 -0500

    main layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -13,6 +13,7 @@ import {
 } from "@/components/ui/sidebar";
 
 import { AppSidebar } from "@/components/app-sidebar";
+import { FileUpload } from "@/components/ui/file-upload";
 import { Separator } from "@/components/ui/separator";
 
 export default function Page() {
@@ -26,49 +27,22 @@ export default function Page() {
           <Breadcrumb>
             <BreadcrumbList>
               <BreadcrumbItem className="hidden md:block">
-                <BreadcrumbLink href="#">
-                  Building Your Application
-                </BreadcrumbLink>
+                <BreadcrumbLink href="#">Web Apps</BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="#">Next.js</BreadcrumbLink>
               </BreadcrumbItem>
               <BreadcrumbSeparator className="hidden md:block" />
               <BreadcrumbItem>
-                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                <BreadcrumbPage>File Upload</BreadcrumbPage>
               </BreadcrumbItem>
             </BreadcrumbList>
           </Breadcrumb>
         </header>
-        {/* <div className="flex flex-1 flex-col gap-4 p-4">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-          </div>
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
-        </div> */}
-
-        <div className="flex flex-1 justify-center items-center">
-          <button
-            type="button"
-            className="w-[800px] aspect-video relative rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-          >
-            <svg
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 48 48"
-              aria-hidden="true"
-              className="mx-auto size-12 text-gray-400"
-            >
-              <path
-                d="M8 14v20c0 4.418 7.163 8 16 8 1.381 0 2.721-.087 4-.252M8 14c0 4.418 7.163 8 16 8s16-3.582 16-8M8 14c0-4.418 7.163-8 16-8s16 3.582 16 8m0 0v14m0-4c0 4.418-7.163 8-16 8S8 28.418 8 24m32 10v6m0 0v6m0-6h6m-6 0h-6"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-            <span className="mt-2 block text-sm font-semibold text-gray-900">
-              Select a video or image file to upload
-            </span>
-          </button>
+        <h1 className="text-4xl font-bold py-8 px-8">Next.js File Upload</h1>
+        <div className="flex flex-1 justify-center pt-24">
+          <FileUpload />
         </div>
       </SidebarInset>
     </SidebarProvider>
